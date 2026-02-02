c9a1ea7ee2ca8e93bce2514da630e3996c8caa68
Jenn Kang <jennkang7@gmail.com>
2026-02-03 10:32:06 +1100

    Refactor summary_stats.sql for improved clarity

--- sql-queries/summary_stats.sql
+++ sql-queries/summary_stats.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE TABLE github-analysis-486123.github_data.summary_stats AS
       , DATE_TRUNC(DATE(TIMESTAMP_SECONDS(c.committer.date.seconds)), MONTH) AS commit_month
       , c.committer.name AS committer_name
     FROM
+      # Cross join to flatten the repo_name array in the commits table
       bigquery-public-data.github_repos.commits AS c
       , UNNEST(repo_name) AS r
     WHERE
@@ -17,17 +18,19 @@ CREATE OR REPLACE TABLE github-analysis-486123.github_data.summary_stats AS
       AND NOT REGEXP_CONTAINS(LOWER(c.committer.name), r'bot|automation|service-account')
   )
 
-  # Create a CTE of repository information, unnesting languages
+  # Create a CTE of repository information, unnesting/flattening language
   , repo_langs AS (
     SELECT 
       repo_name 
       , l.name AS language_name
       , l.bytes
-    FROM bigquery-public-data.github_repos.languages
-    , UNNEST(language) AS l
+    FROM
+      # Cross join to flatten the language array in the languages table
+      bigquery-public-data.github_repos.languages
+      , UNNEST(language) AS l
   )
 
-  # Select aggregated data of repository information to programming language
+  # Select aggregated data of repository information to language information
   SELECT 
     bc.commit_month
     , bc.repo_name
@@ -38,5 +41,4 @@ CREATE OR REPLACE TABLE github-analysis-486123.github_data.summary_stats AS
   FROM base_commits bc
   JOIN repo_langs rl 
     ON bc.repo_name = rl.repo_name
-
   GROUP BY 1, 2, 3, 4;
