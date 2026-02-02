0ba7243fe2ad22e2678005a72b87e78e30fe4712
Jenn Kang <jennkang7@gmail.com>
2026-02-03 10:52:09 +1100

    Update percentile calculation query

--- sql-queries/contributer_segment_pct_calculation.sql
+++ sql-queries/contributer_segment_pct_calculation.sql
@@ -4,9 +4,13 @@ SELECT
   , PERCENTILE_CONT(total_commits, 0.50) OVER() AS p50
   , PERCENTILE_CONT(total_commits, 0.75) OVER() AS p75
 FROM (
-  SELECT committer.name, COUNT(*) as total_commits
-  FROM bigquery-public-data.github_repos.commits
-  WHERE TIMESTAMP_SECONDS(committer.date.seconds) BETWEEN '2017-11-01' AND '2022-11-30'
+  SELECT
+    committer.name
+    , COUNT(*) AS total_commits
+  FROM
+    bigquery-public-data.github_repos.commits
+  WHERE
+    TIMESTAMP_SECONDS(committer.date.seconds) BETWEEN '2017-11-01' AND '2022-11-30'
   GROUP BY 1
 )
 LIMIT 1;
